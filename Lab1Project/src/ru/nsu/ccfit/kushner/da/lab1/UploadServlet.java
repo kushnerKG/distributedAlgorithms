c8ed857ae5df4bc9a19d69e2e43efe1a41d40b8f
Kushner Konstantin <kushner.k.g@gmail.com>
2016-11-18 18:37:55 +0700

    в память считывается картинка в bse64

--- Lab1Project/src/ru/nsu/ccfit/kushner/da/lab1/UploadServlet.java
+++ Lab1Project/src/ru/nsu/ccfit/kushner/da/lab1/UploadServlet.java
@@ -1,5 +1,8 @@
 package ru.nsu.ccfit.kushner.da.lab1;
 
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.ServletException;
@@ -12,6 +15,7 @@ import javax.servlet.http.Part;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
 
 import java.io.*;
 import java.util.Iterator;
@@ -39,7 +43,81 @@ public class UploadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response) throws ServletException, IOException {
 
-        if (!ServletFileUpload.isMultipartContent(request)) {
+
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload();
+
+// Parse the request
+        FileItemIterator iter = null;
+        try {
+            iter = upload.getItemIterator(request);
+
+            while (iter.hasNext()) {
+                FileItemStream item = iter.next();
+                String fileName = item.getName();
+                String name = item.getFieldName();
+                InputStream stream = item.openStream();
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                byte[] buffer = new byte[1024*1024];
+                while (true){
+                    // читаем данные в буфер
+                    int readBytesCount = stream.read(buffer);
+                    if (readBytesCount == -1) {
+                        // данные закончились
+                        break;
+                    }
+                    if (readBytesCount > 0) {
+                        // данные были считаны - есть, что записать
+                        baos.write(buffer, 0, readBytesCount);
+                    }
+                }
+                baos.flush();
+                baos.close();
+                byte[] data = baos.toByteArray();
+
+
+                String uploadPath = getServletContext().getRealPath("./") + File.separator + UPLOAD_DIRECTORY;
+
+
+                File uploadDir = new File(uploadPath);
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdir();
+                }
+//
+//                String filePath = uploadPath + File.separator + fileName.substring(0, fileName.lastIndexOf(".")) + ".txt";
+//                File storeFile = new File(filePath);
+//
+//                FileOutputStream fos = new FileOutputStream(storeFile);
+                String sCert = javax.xml.bind.DatatypeConverter.printBase64Binary(data);
+
+                //fos.write(sCert.getBytes());
+
+
+                request.setAttribute("message", data.length);
+
+                getServletContext().getRequestDispatcher("/uploadPage.jsp").forward(
+                        request, response);
+
+                /*if (item.isFormField()) {
+                    System.out.println("Form field " + name + " with value "
+                            + Streams.asString(stream) + " detected.");
+                } else {
+                    System.out.println("File field " + name + " with file name "
+                            + item.getName() + " detected.");
+                    // Process the input stream
+
+                }*/
+            }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+        /*if (!ServletFileUpload.isMultipartContent(request)) {
 
             PrintWriter writer = response.getWriter();
             writer.println("Error: 表单必须包含 enctype=multipart/form-data");
@@ -53,6 +131,8 @@ public class UploadServlet extends HttpServlet {
 
 
         //factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+        factory.setRepository(null);
 
         ServletFileUpload upload = new ServletFileUpload(factory);
 
@@ -88,12 +168,12 @@ public class UploadServlet extends HttpServlet {
             }
         } catch (Exception ex) {
             request.setAttribute("message",
-                    "错误信息: " + ex.getMessage());
+                    "ERROR: " + ex.getMessage());
         }
 
         getServletContext().getRequestDispatcher("/uploadPage.jsp").forward(
                 request, response);
-    }
+    }*/
 
 
 
