5c7a60102fe23d3c1c70d9f309e8ba4a6160b1b8
Kushner Konstantin <kushner.k.g@gmail.com>
2016-11-18 14:16:05 +0700

    sbdfsbfdbd

---
+++ Lab1Project/src/ru/nsu/ccfit/kushner/da/lab1/MainServlet.java
@@ -0,0 +1,28 @@
+package ru.nsu.ccfit.kushner.da.lab1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by konstantin on 11.11.16.
+ */
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+
+        super.doPost(req, resp);
+    }
+}
